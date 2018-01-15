a6acf7656b6ded0ee4fde92951df84cb01477c0f
Marius Reusch <marius.reusch@gmail.com>
2018-01-15 21:22:36 +0100

    organize imports

--- src/test/java/ch/zuehlke/mare/java9collections/Java9ListTest.java
+++ src/test/java/ch/zuehlke/mare/java9collections/Java9ListTest.java
@@ -1,13 +1,10 @@
 package ch.zuehlke.mare.java9collections;
 
-import org.assertj.core.api.ThrowableAssert;
 import org.assertj.core.api.ThrowableAssert.ThrowingCallable;
 import org.junit.Test;
 
 import java.util.List;
-import java.util.Map;
 
-import static java.util.Map.entry;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
